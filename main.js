5a36096a6eb2c8efd9c97fa929cb4ece742c918a
egrizq <rizq.ramadhan17@gmail.com>
2023-11-24 19:04:19 +0700

    new update training & certifications

--- main.js
+++ main.js
@@ -7,3 +7,43 @@ project2.forEach(function(project2) {
         this.parentElement.remove()
     })
 })
+
+function dasarData() {
+    window.open("https://www.dicoding.com/certificates/98XWVRKELPM3", "dicoding","width=800px height=500px")
+}
+
+function sql() {
+    window.open("https://www.dicoding.com/certificates/JLX1DOWO6Z72", "dicoding","width=800px height=500px")
+}
+
+function python() {
+    window.open("https://www.dicoding.com/certificates/98XWVRED0PM3", "dicoding","width=800px height=500px")
+}
+
+function analisisPython() {
+    window.open("https://www.dicoding.com/certificates/N9ZO56O2DPG5", "dicoding","width=800px height=500px")
+}
+
+function ml() {
+    window.open("https://www.dicoding.com/certificates/JMZV1DDJ3XN9", "dicoding","width=800px height=500px")
+}
+
+function js() {
+    window.open("https://www.dicoding.com/certificates/L4PQGO36OZO1", "dicoding","width=800px height=500px")
+}
+
+function projek() {
+    window.open("https://www.dicoding.com/certificates/53XEN57KVXRN", "dicoding","width=800px height=500px")
+}
+
+function pengembang() {
+    window.open("https://www.dicoding.com/certificates/2VX3J9R9VPYQ", "dicoding","width=800px height=500px")
+}
+
+function introtf() {
+    window.open("https://www.coursera.org/account/accomplishments/certificate/7YJCTHTKWKHS", "coursera","width=800px height=500px")
+}
+
+function excel() {
+    window.open("https://www.udemy.com/certificate/UC-82d677a2-25d1-4a24-9b10-afdf2b74668e/", "coursera","width=800px height=500px")
+}
